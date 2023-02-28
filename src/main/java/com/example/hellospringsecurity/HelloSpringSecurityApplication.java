83a4ab04092052c0872ca4c28ed0532d8a16237b
vuongNguyenVg <vuongnguyen332003@gmail.com>
2023-02-28 11:29:30 +0700

    SpringSecurity

---
+++ src/main/java/com/example/hellospringsecurity/HelloSpringSecurityApplication.java
@@ -0,0 +1,20 @@
+package com.example.hellospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloSpringSecurityApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(HelloSpringSecurityApplication.class, args);
+   }
+   @GetMapping("/users")
+   public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name){
+      return String.format("Hello %s!", name);
+   }
+}
